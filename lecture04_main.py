9236fa3c605764eb38216980ea5b5980d90956fc
Karotte-23 <kgkarotte23can@gmail.com>
2025-10-23 16:09:45 +0900

    main.pyを更新

--- lecture04_main.py
+++ lecture04_main.py
@@ -3,4 +3,4 @@ import audio_to_text
 
 if __name__ == "__main__":
     audio_file_path = recorder.record_audio
-    audio_to_text.audio_data_to_text(audio_file_path)
+    audio_to_text.audio_data_to_text()
